da6bdbcdcab2bea51926bb8453c95e84e274d919
TarasMysiura <tarasmysiura@gmail.com>
2023-05-16 11:46:21 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -12,7 +12,7 @@ refs.btnStop.style.height = '50px';
 
 refs.btnStop.setAttribute(`disabled`, true);
 
-// let colorTimer = null;
+let colorTimer = null;
 refs.btnStart.addEventListener('click', colorChanger);
 refs.btnStop.addEventListener('click', colorChangerStop);
 
@@ -29,14 +29,6 @@ function colorChangerStop() {
   refs.btnStop.setAttribute(`disabled`, true);
   clearInterval(colorTimer);
 }
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-console.log(refs.btnStart.textContent);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
